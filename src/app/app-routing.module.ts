b0a3bd00fa451cb0273b3caac07d8b2fa2d792d1
Deivid <deividtlima@gmail.com>
2023-10-27 17:05:05 -0300

    feat: 🎸 criacao da tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './login/login-form/login-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    { path: '', redirectTo: '/login-form', pathMatch: 'full' },
+    { path: 'login-form', component: LoginFormComponent },
+    { path: '**', component: LoginFormComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
